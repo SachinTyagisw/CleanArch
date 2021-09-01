841c1d9077eb4ada0f46a3330c7ed765e041bca4
Sachin Tyagi <sachin.tyagi@optym.com>
2021-09-01 14:13:06 +0530

    Add Schedule Domain Model.

---
+++ CleanArch/CleanArch.Domain/Entities/Schedule.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanArc.Domain.Entities
+{
+    public class Schedule
+    {
+        public int Id { get; set; }
+        public string Source { get; set; }
+        public string Destination { get; set; }
+    }
+}
